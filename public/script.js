463c5a7686fc2c44ce5834917987c0a30488a226
Benson Lee <BensonL1334@gmail.com>
2023-02-27 22:48:32 -0500

    Testing front-end

--- public/script.js
+++ public/script.js
@@ -6,14 +6,15 @@ const getJudoBtn = document.getElementById('getJudoBtn')
 const results = document.getElementById('results')
 
 getJudoBtn.addEventListener('click', async () => {
-  await fetch('/')
-    .then(res => res.json())
-    .then(data => {
-      results.innerHTML = ''
-      data.forEach(judo => {
-        const judoElement = document.createElement('div')
-        judoElement.innerHTML = `<h2>${judo.name}</h2><p>${judo.type}</p>`
-        results.appendChild(judoElement)
-      })
-    })
+  const response = await fetch('/')
+  const judoList = await response.json()
+  console.log(judoList);
+
+  //   results.innerHTML = ''
+  //   data.forEach(judo => {
+  //     const judoElement = document.createElement('div')
+  //     judoElement.innerHTML = `<h2>${judo.name}</h2><p>${judo.type}</p>`
+  //     results.appendChild(judoElement)
+  //   })
+  // })
 })
